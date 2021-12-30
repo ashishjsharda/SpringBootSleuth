91944db721f46370bd22ffc5df0aa5ef01019f2a
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-30 05:48:52 -0500

    first commit

---
+++ src/main/java/com/example/sleuthdemo/SleuthdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.sleuthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SleuthdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthdemoApplication.class, args);
+    }
+
+}
