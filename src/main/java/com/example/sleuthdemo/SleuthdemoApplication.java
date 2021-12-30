37ea78cb338f6821891dd76cdb8e05eec4b9850c
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-30 06:24:09 -0500

    Update Logger

--- src/main/java/com/example/sleuthdemo/SleuthdemoApplication.java
+++ src/main/java/com/example/sleuthdemo/SleuthdemoApplication.java
@@ -1,12 +1,17 @@
 package com.example.sleuthdemo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
 public class SleuthdemoApplication {
 
+    private static final Logger LOG=Logger.getLogger(SleuthdemoApplication.class.getName());
     public static void main(String[] args) {
         SpringApplication.run(SleuthdemoApplication.class, args);
     }
